679f59f8f3b4392f923fb8d1df77567aaf3f4fbe
Christian Broms <cb@christianbroms.com>
2019-01-19 20:52:44 -0500

    redesign landing page

--- magcInteractiveMap/views.py
+++ magcInteractiveMap/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from mapApplication.models import Tour, TourLocation
+from django.core import serializers
 
 def index(request):
-    return render(request, 'index.html')
+
+    tour = Tour.objects.filter(name="Question Guide")[0]
+    tourLocations = TourLocation.objects.filter(tour=tour)
+    tourText = serializers.serialize('json', tourLocations)
+
+    return render(request, 'index.html', {'locationsText': tourText})
